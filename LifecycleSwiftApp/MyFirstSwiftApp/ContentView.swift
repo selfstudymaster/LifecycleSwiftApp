d2949dd8e9f34a88f6dfc985be178adae4a045e3
selfstudymaster <webselfmaster@gmail.com>
2020-12-11 16:33:50 +0900

    This is the first commit LifecycleSwiftApp.clone

---
+++ LifecycleSwiftApp/MyFirstSwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftApp
+//
+//  Created by mac on 2020/12/09.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi, I'm Genius! How are you?")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
